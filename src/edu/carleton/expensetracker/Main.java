d8dccdfbe0d40416d89715e569e1c0ebbd706348
Tao Liu <liut2@carleton.edu>
2016-05-23 22:35:13 -0500

    added create scenes functions which create new scene on button events

--- src/edu/carleton/expensetracker/Main.java
+++ src/edu/carleton/expensetracker/Main.java
@@ -6,17 +6,46 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        //primaryStage.setTitle("Personal Expense Tracker");
+        primaryStage.setTitle("Personal Expense Tracker");
+        Scene scene = createExpenseScene();
         // set the default window size
-        //primaryStage.setScene(new Scene(root, 900, 600));
-        //primaryStage.show();
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
+    /*
+    This function handles the create expense scene.
+    Tao writes here.
+     */
+    public Scene createExpenseScene(){
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("/fxml/createExpense.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new Scene(root, 900, 600);
+    }
+
+    /*
+    This function handles the view expense scene.
+    Xi writes here.
+     */
+    public Scene viewExpenseScene(){
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("/fxml/viewExpense.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new Scene(root, 900, 600);
+    }
 
     public static void main(String[] args) {
         launch(args);
