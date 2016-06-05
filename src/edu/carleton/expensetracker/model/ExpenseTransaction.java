22cd984e68618fd2c3ccc1c73c1528dba5053b5a
Xi Chen <chenx2@carleton.edu>
2016-06-04 19:47:06 -0500

    added comments

--- src/edu/carleton/expensetracker/model/ExpenseTransaction.java
+++ src/edu/carleton/expensetracker/model/ExpenseTransaction.java
@@ -2,6 +2,7 @@ package edu.carleton.expensetracker.model;
 
 /**
  * Created by chenx2 on 6/1/2016.
+ * This class represents the expense transaction
  */
 public class ExpenseTransaction extends Transaction{
     public ExpenseTransaction(){
--- src/edu/carleton/expensetracker/model/IncomeTransaction.java
+++ src/edu/carleton/expensetracker/model/IncomeTransaction.java
@@ -2,6 +2,7 @@ package edu.carleton.expensetracker.model;
 
 /**
  * Created by chenx2 on 6/1/2016.
+ * This class represent the income transaction
  */
 public class IncomeTransaction extends Transaction {
     public IncomeTransaction(){
